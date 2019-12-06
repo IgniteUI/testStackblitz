4e3356a653f426abb1da5f3eddf23cefb69331f8
mddifilippo89 <mddifilippo89@comcast.net>
2019-12-06 12:18:45 -0500

    mdd-update
    mdd-update

--- my-app-stackblitz2/src/index.tsx
+++ my-app-stackblitz2/src/index.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+import { IgrCategoryChartModule } from 'igniteui-react-charts/ES5/igr-category-chart-module';
+import { IgrCategoryChart} from 'igniteui-react-charts/ES5/igr-category-chart';
+import { IIgrCategoryChartProps} from 'igniteui-react-charts/ES5/igr-category-chart';
+ 
+interface IAppProps {
+}
+interface IAppState {
+    data: any[];
+    name: string
+}
+IgrCategoryChartModule.register();
+ 
+class App extends Component<IAppProps, IAppState> {
+ 
+public data: any;
+  constructor(props: Readonly<IAppProps>) {
+    super(props);
+    this.state = {
+            data: [
+              { Label: "A", Value: 5, Value2: 4 },
+              { Label: "B", Value: 8, Value2: 2 },
+              { Label: "C", Value: 2, Value2: 3 },
+              { Label: "D", Value: 4, Value2: 4 },
+              { Label: "E", Value: 5, Value2: 5 },
+              { Label: "F", Value: 6, Value2: 4 },
+              { Label: "G", Value: 3, Value2: 4 }
+            ],
+            name: 'React'
+        };
+    
+  }
+ 
+  render() {
+ 
+    return (
+      <div>
+            {/* <Hello name={this.state.name} /> */}
+            <IgrCategoryChart yAxisMinimumValue={0}
+                              chartTitle="test"
+                              width="500px"
+                              height="500px"
+                              dataSource={this.state.data} />
+          </div>
+    );
+  }
+}
+ 
+render(<App />, document.getElementById('root'));
