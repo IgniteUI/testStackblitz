8498d3960656eab6c9567518dce48e848de47a04
mddifilippo89 <mddifilippo89@comcast.net>
2020-02-20 11:12:25 -0500

    Update App.tsx

--- my-code-box-test/src/App.tsx
+++ my-code-box-test/src/App.tsx
@@ -39,6 +39,9 @@ class App extends Component<IAppProps, IAppState> {
                       width="500px"
                       height="500px"
                       dataSource={this.state.data} />
+    <a target="_blank" href="https://codesandbox.io/s/github/IgniteUI/testStackblitz/tree/master/my-code-box-test?from-embed">
+        <img alt="Edit fbusv" src="https://codesandbox.io/static/img/play-codesandbox.svg"/>
+    </a>
   </div>
   );
 }
