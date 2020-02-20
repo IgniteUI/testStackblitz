9a1f9583d4e4e2a19852b0d0235ad6592a640ad1
mddifilippo89 <mddifilippo89@comcast.net>
2020-02-20 10:07:32 -0500

    Add files via upload

---
+++ my-code-box-test/src/App.tsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { IgrCategoryChartModule } from 'igniteui-react-charts'
+import { IgrCategoryChart} from 'igniteui-react-charts'
+
+interface IAppProps {
+}
+interface IAppState {
+    data: any[];
+    name: string
+}
+IgrCategoryChartModule.register();
+
+class App extends Component<IAppProps, IAppState> {
+
+  public data: any;
+  constructor(props: Readonly<IAppProps>) {
+    super(props);
+    this.state = {
+            data: [
+              { Label: "A", Value: 5, Value2: 4 },
+              { Label: "B", Value: 8, Value2: 2 },
+              { Label: "C", Value: 2, Value2: 3 },
+              { Label: "D", Value: 4, Value2: 4 },
+              { Label: "E", Value: 5, Value2: 5 },
+              { Label: "F", Value: 6, Value2: 4 },
+              { Label: "G", Value: 3, Value2: 4 }
+            ],
+            name: 'React'
+        };
+    
+  }
+  render() {
+  return (
+    <div>
+    {/* <Hello name={this.state.name} /> */}
+    <IgrCategoryChart yAxisMinimumValue={0}
+                      chartTitle="test"
+                      width="500px"
+                      height="500px"
+                      dataSource={this.state.data} />
+  </div>
+  );
+}
+}
+
+export default App;
